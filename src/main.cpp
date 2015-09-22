d3c1f51ce987c9db1e15e7ab40da718a7b33e358
amacdiarmid <amacdiarmid@hotmail.co.uk>
2015-09-22 16:41:44 +0100

    SDL init

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,13 @@
 
 int main(int argc, char * arg[])
 {
+	//init everything - SDL, if it is nonZero we have a problem
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "ERROR SDL_Init " << SDL_GetError() << std::endl;
+
+		return -1;
+	}
+
     return 0;
 }
