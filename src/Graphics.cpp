dd91e13f7efbb35b1ebcbfcc38c6fac0ce58ba8e
amacdiarmid <amacdiarmid@hotmail.co.uk>
2015-09-23 10:22:37 +0100

    4.4
    calling initOpenGL

---
+++ src/Graphics.cpp
@@ -0,0 +1,24 @@
+#include "Graphics.h"
+#include "Common.h"
+
+//function to initalise OpenGL
+void initOpenGL()
+{
+	//smopth shading
+	glShadeModel(GL_SMOOTH);
+
+	//clear the background to black
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	//clear the depth buffer to 1.0
+	glClearDepth(1.0f);
+
+	//enable depth testing
+	glEnable(GL_DEPTH_TEST);
+
+	//the depth test to use
+	glDepthFunc(GL_LEQUAL);
+
+	//turn on best perspective correction
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
