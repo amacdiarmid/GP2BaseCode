db530595e729d524601a8e43825d76fe3ccf9f6a
amacdiarmid <amacdiarmid@hotmail.co.uk>
2015-09-23 10:31:31 +0100

    5.1
    render function

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 
 void render()
 {
-
+	//set the clear colour background 
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	//clear the color and depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
 }
 
 void update()
