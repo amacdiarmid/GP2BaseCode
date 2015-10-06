5fceb0a61e249a0f5810f88f720b1f362604b21f
amacdiarmid <amacdiarmid@hotmail.co.uk>
2015-10-06 16:08:56 +0100

    2
    loading, compile and linking shaders

---
+++ include/Shader.h
@@ -0,0 +1,16 @@
+#ifndef Shader_h
+#define Shader_h
+
+#include "Common.h"
+
+enum SHADER_TYPE
+{
+	VERTEX_SHADER = GL_VERTEX_SHADER,
+	FRAGMENT_SHADER = GL_FRAGMENT_SHADER
+};
+
+GLuint loadShaderFromMemory(const char *pMem, SHADER_TYPE shaderType);
+GLuint loadShaderFromFile(const std::string& filename, SHADER_TYPE shaderType);
+bool checkForCompilerErrors(GLuint shaderProgram);
+
+#endif
